c018558415d6e5ac0a411636a7bf95c11a6ca0df
iaroslav_shavrov <iaroslav_shavrov@practice.relex.ru>
2025-02-20 02:59:58 +0300

    home. Added a product and adaptive section

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import ProductSection from '../components/ProductSection';
+
+const Wrapper = styled('div')``;
+
+export default function Home() {
+    return (
+        <Wrapper>
+            <ProductSection />
+        </Wrapper>
+    )
+}
